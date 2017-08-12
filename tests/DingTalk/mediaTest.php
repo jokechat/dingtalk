fd4e90c5fb726775c1ada5f2ea3151debad4a528
jokechat <jokechat@qq.com>
2017-08-12 11:07:44 +0800

    修改上传文件测试用例

--- tests/DingTalk/mediaTest.php
+++ tests/DingTalk/mediaTest.php
@@ -6,7 +6,7 @@ class MediaTest extends PHPUnit_Framework_TestCase
     public function testUpload()
     {
         $media            = new Media();
-        $media_path       =   "/Users/jokechat/Documents/logo.png";
+        $media_path       =   "logo.png";
         $result           = $media->media_upload("image", $media_path);
         $this->assertArrayHasKey("media_id", $result);
     }
